e91feabf12f77e950eb0d75ca280176344d20735
Bəhmən Cəfərov <30909255+Bahman-Jafarov@users.noreply.github.com>
2020-12-14 17:36:54 +0400

    Managing User Roles

--- TRMDesktopUI.Library/Api/IUserEndpoint.cs
+++ TRMDesktopUI.Library/Api/IUserEndpoint.cs
@@ -6,6 +6,9 @@ namespace TRMDesktopUI.Library.Api
 {
     public interface IUserEndpoint
     {
+        Task AddUserToRole(string userId, string role);
         Task<List<UserModel>> GetAll();
+        Task<Dictionary<string, string>> GetAllRoles();
+        Task RemoveUserFromRole(string userId, string roleName);
     }
 }
