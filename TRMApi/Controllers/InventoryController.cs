256da6700c1ddd06d03ab619648820e044a17d42
Bəhmən Cəfərov <30909255+Bahman-Jafarov@users.noreply.github.com>
2020-12-18 16:50:47 +0400

    Added New API and Moved Over Functionality

---
+++ TRMApi/Controllers/InventoryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class InventoryController : ControllerBase
+    {
+        [Authorize(Roles = "Admin,Manager")]
+        public List<InventoryModel> Get()
+        {
+            InventoryData data = new InventoryData();
+            return data.GetInventory();
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        public void Post(InventoryModel item)
+        {
+            InventoryData data = new InventoryData();
+            data.SaveInventoryRecord(item);
+        }
+    }
+}
